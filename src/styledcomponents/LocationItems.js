2d749bd08c2796d7f899c8170f5917ba7af58e60
Kevin <kevbf1999@gmail.com>
2021-09-06 12:54:09 -0300

    Changed Maps width from 100% to 50%, added property border: none, added d-flex to InfoMaps, structure reorganized

--- src/styledcomponents/LocationItems.js
+++ src/styledcomponents/LocationItems.js
@@ -1,18 +1,36 @@
 import styled from "styled-components";
 
 export const Maps = styled.iframe`
-    width: 100%;
+    width: 50%;
     height: 500px;
     margin-bottom: 0;
+    border: none;
 `;
 
 export const InfoContainer = styled.div`
     display:flex;
     justify-content: space-around;
+    background: rgba(255, 255, 255, .7);
 `;
 
 export const InfoMaps = styled.div`
-    font-size: 24px;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
     line-height: 35px;
-    padding: 30px
+    padding: 30px;
+
+    /* border: 2px solid #ff9900; */
+
+    h4{
+            text-decoration: underline;
+            font-size: 24px;
+    }
+    p{
+            margin-bottom: 20px;
+            font-size: 18px;
+    }
+    
 `;
