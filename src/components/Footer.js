d2a977e1bf1c07bb9a0b93dd90ba04a69e6de532
Kevin <kevbf1999@gmail.com>
2021-09-06 07:34:47 -0300

    Deleted component Maps from Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { FooterContainer, FooterInfo } from '../styledcomponents/FooterContainer'
-import { Maps } from '../styledcomponents/LocationItems'
 import { Title } from '../styledcomponents/News'
 
 function Footer() {
     return (
         <FooterContainer>
-            <Maps title='maps' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d204.86874525634593!2d-58.212070950124385!3d-34.75811369957333!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a32f37e2eeb25d%3A0xe38e34bf5e0b39cf!2sEscuela%20de%20educaci%C3%B3n%20media%20N%C2%B0%205%20-%20Escuela%20N%C2%B0%209!5e0!3m2!1ses-419!2sar!4v1623453131374!5m2!1ses-419!2sar" frameborder="0"></Maps>
             <FooterInfo> <Title variant="footer">Secundaria 5 Berazategui</Title> </FooterInfo>
         </FooterContainer>
     )
